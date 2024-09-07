006c1bbee0962422d5573d47dd6f4d12722a8e62
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-09-07 10:13:55 +0545

    login controller ✅

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -48,3 +48,27 @@ export const signup = async (req, res, next) => {
     });
   }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) return res.status(404).json({ message: 'User not found' });
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return res.status(401).json({ message: 'Invalid email or password' });
+      
+      const token = jwt.sign({ id: validUser._id, role: validUser.role }, '23787234hello123heiihelosdufoi');
+      const { password: hashedPassword, ...rest } = validUser._doc;
+      
+      const expiryDate = new Date(Date.now() + 3600000); // 1 h from now
+      
+      res
+        .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+        .status(200)
+        .json({ ...rest, tokenExpiration: expiryDate }); // Sending token expiration date to client
+        
+      console.log('User signed in successfully');
+    } catch (error) {
+      next(error);
+    }
+  };
